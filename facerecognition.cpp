f17ed0e78d2eab899f8dc2c1023fe59cec1c9995
eduardob <angelsoulsword@gmail.com>
2017-01-14 15:07:06 -0500

    left bugs of the tree

--- facerecognition.cpp
+++ facerecognition.cpp
@@ -2,13 +2,37 @@
 #include "CImg.h"
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
+#include "SATree.h"
 using namespace cimg_library;
 using namespace std;
 #include "haar.hpp"
 #include "haar.cpp"
+//template <typename T,unsigned S>
+//inline unsigned arraysize(const T (&v)[S]) { return S; }
 
+struct imageindex
+{
+	string myroute;
+	double* importantPoints;
+	int size;
+	//int lenght[];
+};
+vector<imageindex> Index;
+Dis dd(imageindex a,imageindex b )
+{
+	Dis sum = 0;
+	for(int i=0;i<a.size;i++)
+	{
+		sum += pow(b.importantPoints[i]-a.importantPoints[i],2);
+	}
+	cout<<sqrt(sum);
+	return sqrt(sum);
+}
+//SATree<vector<float>>::FunDis distance = dd;
 double* idwt(int rows,int cols,double myimage[], CImg<unsigned char> image)
 {
 	double matriz[rows+1][cols+1];
@@ -55,6 +79,8 @@ void indexar()
 {
 	int aux = 0;
 	int numero = 1;
+	
+	imageindex* recover = new imageindex[165];
 	//double** myimage = new double*[165];
 	char letra = 'a';
     string post;
@@ -75,7 +101,7 @@ void indexar()
 		sprintf(numstr, "%d", numero);
 		post = letrastring + numstr +".pgm";
 		string direccion = pre + post;
-		cout<<direccion<<endl;
+		//cout<<direccion<<endl;
 		CImg<unsigned char> image(direccion.c_str());//,visu(500,400,1,3,0);
 		CImgDisplay main_disp(image,direccion.c_str());
 		int height, width; 
@@ -86,9 +112,15 @@ void indexar()
 		//
 		//myimage[aux] = new double[rows*cols];
 		//
-		double myimage[(rows+1)*(cols+1)];
+		//double myimage[(rows+1)*(cols+1)];
+		double* myimage = new double[(rows+1)*(cols+1)];
 
 		idwt(rows,cols,myimage,image);	
+		recover[aux].myroute = direccion;
+		cout<<recover[aux].myroute<<endl;
+		recover[aux].importantPoints = myimage;
+		recover[aux].size= (rows+1)*(cols+1);
+		Index.push_back(recover[aux]);
 		cout<<"ey"<<endl;	
 		
 		
@@ -97,14 +129,40 @@ void indexar()
 			main_disp.wait();
 		}*/
 		
-		//delete [] myimage;
+		delete [] myimage;
 		numero++;
 		aux++;
 	}
+	delete [] recover;
+	return;
 }
 
+void comparation()
+{
+	///ACA ESTA EL ERROR SUPONGO QUE SERA LA FUNCION DISTANCIA DD
+	SATree<imageindex>::FunDis distance = dd;
+	SATree<imageindex> tree(dd, Index);
+	/*string direccionnueva="/home/eduardob/Desktop/Aed_Final/Face_Similarity/yalefaces/yalefaces/a1.pgm";
+	CImg<unsigned char> image(direccionnueva.c_str());
+	CImgDisplay main_disp(image,direccionnueva.c_str());
+	int height, width; 
+	height = image.height();
+	width = image.width();
+	int rows =(int) height;
+	int cols =(int) width;
+	double* mynewimage = new double[(rows+1)*(cols+1)];
+	idwt(rows,cols,mynewimage,image);
+	imageindex comparatebitch;
+	comparatebitch.myroute=direccionnueva;
+	comparatebitch.size = (rows+1)*(cols+1);
+	comparatebitch.importantPoints = mynewimage;
+	auto res2 = tree.NNSearch(comparatebitch,10);
+	cout<<res2.size();*/
+}
 int main()
 {
 	indexar();
+	cout<<Index.size();
+	comparation();
 	return 0;
 }
